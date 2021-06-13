4ed60f6602ae372b871429457b3572ce3d0740d1
mathieu <mmehran@student.42.fr>
2021-06-14 01:56:37 +0200

    f

--- hooks.c
+++ hooks.c
@@ -6,7 +6,7 @@
 /*   By: mmehran <mmehran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 14:28:01 by mmehran           #+#    #+#             */
-/*   Updated: 2021/06/14 01:33:47 by mmehran          ###   ########.fr       */
+/*   Updated: 2021/06/14 01:56:16 by mmehran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	close_program(t_vars *vars)
 	free(vars->mlx);
 	exit(0);
 }
+
 int	key_hook(int keycode, t_vars *vars)
 {
 	if (keycode == KEY_ESC)
